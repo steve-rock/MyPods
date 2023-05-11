719309da96fc2be5074701e42b95a450859b1145
quynhMI <quynhMI@gmail.com>
2023-05-11 11:38:03 +0700

    add framework

---
+++ SwiftyAds.xcframework/ios-arm64_x86_64-simulator/SwiftyAds.framework/Headers/SwiftyAds.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyAds.h
+//  SwiftyAds
+//
+//  Created by MinhNH on 04/05/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyAds.
+FOUNDATION_EXPORT double SwiftyAdsVersionNumber;
+
+//! Project version string for SwiftyAds.
+FOUNDATION_EXPORT const unsigned char SwiftyAdsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyAds/PublicHeader.h>
+
+
